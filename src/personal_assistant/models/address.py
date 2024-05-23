feae89c037b9bbb33ec1d0733935e4abad1648de
Nataliia Maslova <nataliiamaslovaneo@gmail.com>
2024-05-23 12:02:19 +0200

    updated Address class

--- src/personal_assistant/models/address.py
+++ src/personal_assistant/models/address.py
@@ -1,12 +1,19 @@
+
+"""
+This is the Address class for the personal assistant application. It parses the address string arguments, 
+validates the data from the user, and returns the address.
+"""
+import re
+from typing import Optional 
 class Address:
-    def __init__(self, address_str):
-        self.street = None
-        self.house_number = None
-        self.apartment_number = None
-        self.city = None
-        self.state = None
-        self.postal_code = None
-        self.country = None
+    def __init__(self, address_str: str) -> None:
+        self.street: Optional[str] = None
+        self.house_number: Optional[str] = None
+        self.apartment_number: Optional[str] = None
+        self.city: Optional[str] = None
+        self.state: Optional[str] = None
+        self.postal_code: Optional[str] = None
+        self.country: Optional[str] = None
         self.parse_address(address_str)
         self.validate()
 
@@ -15,15 +22,28 @@ class Address:
         if len(parts) > 0:
             self.street = parts[0].strip()
         if len(parts) > 1:
-            self.house_number = parts[1].strip()
+            potential_house_number = parts[1].strip()
+            if re.match(r'^\d[\d\w]*$', potential_house_number):
+                self.house_number = potential_house_number
+            else:
+                raise ValueError("Недійсний номер будинку: має починатися з цифри та містити лише цифри та літери.")
         if len(parts) > 2:
-            self.apartment_number = parts[2].strip()
+            potential_apartment_number = parts[2].strip()
+            if re.match(r'^[\d\w]+$', potential_apartment_number):
+                self.apartment_number = potential_apartment_number
+            else:
+                raise ValueError("Невірний номер квартири: має містити лише цифри та літери.")
+
         if len(parts) > 3:
             self.city = parts[3].strip()
         if len(parts) > 4:
             self.state = parts[4].strip()
         if len(parts) > 5:
-            self.postal_code = parts[5].strip()
+            potential_postal_code = parts[5].strip()
+            if re.match(r'^\d+$', potential_postal_code):
+                self.postal_code = potential_postal_code
+            else:
+                 raise ValueError("Недійсний поштовий індекс: має містити лише цифри.")
         if len(parts) > 6:
             self.country = parts[6].strip()
 
@@ -37,23 +57,4 @@ class Address:
 
     def __str__(self):
         parts = [self.street, self.house_number, self.apartment_number, self.city, self.state, self.postal_code, self.country]
-        address_string = ", ".join(filter(lambda x: x is not None, parts))
-        return address_string
-
-
-"""
-#приклад використання
-
-if __name__ == "__main__":
-
-    input_address = input(Address.get_input_format() + "\n")
-    try:
-        address = Address(input_address)
-        print("Адреса успішно введена:")
-        print(address)
-    except ValueError as e:
-        print("Помилка валідації адреси:")
-        print(e)
-
-"""
-
+        return ", ".join(filter(None, parts))
