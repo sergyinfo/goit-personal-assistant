1affce5b434d7c3fa3e3a08e8c3992845a0b190d
Sergey Porotikov <sporotikov@cordial.com>
2024-05-25 16:33:54 +0300

    fixed contacts search

--- src/personal_assistant/services/address_book.py
+++ src/personal_assistant/services/address_book.py
@@ -56,8 +56,9 @@ class AddressBook:
         """
         Checks if a contact matches the given keyword.
         """
-        is_any = field == 'any'
+        is_any = field == 'any' or field is None
         keyword = keyword.lower()
+
         if ('name' == field or is_any) and keyword in contact.name.lower():
             return True
         if ('phone' == field or is_any) and any(keyword in str(phone).lower() for phone in contact.phone_numbers):
